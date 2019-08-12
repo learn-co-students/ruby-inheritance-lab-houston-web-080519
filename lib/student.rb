cf2cac392ebaee57b7c297cbc36282abac9c68b2
Michael Evans <mmevansjr@gmail.com>
2019-08-12 10:57:36 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
 
+    attr_accessor :first_name, :last_name, :knowledge
+
+    def initialize()
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge.push(string)
+    end
 end
